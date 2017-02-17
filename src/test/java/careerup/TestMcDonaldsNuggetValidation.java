2520aba7ff631b925792cc2cc6ce8c37e29f2daa
tolgaduzenli <4467TLG339tlg>
2017-02-17 19:53:09 +0100

    Added test cases.

--- src/test/java/careerup/TestMcDonaldsNuggetValidation.java
+++ src/test/java/careerup/TestMcDonaldsNuggetValidation.java
@@ -4,6 +4,9 @@ import hackerrank.McDonaldsNuggetValidation;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Tolga Duzenli on 2/17/17.
  */
@@ -12,13 +15,24 @@ public class TestMcDonaldsNuggetValidation {
     private static McDonaldsNuggetValidation mcDonaldsNuggetValidation;
 
     @BeforeClass
-    public static void setup(){
+    public static void setup() {
         mcDonaldsNuggetValidation = new McDonaldsNuggetValidation();
     }
 
     @Test
-    public void testWithFirstHundredNumbers(){
-        for(int i = 0; i<=100;i++){
+    public void testWithNegtiveNumber() {
+        assertFalse(mcDonaldsNuggetValidation.isNuggetNumberValid(-5));
+    }
+
+    @Test
+    public void testWithZero() {
+        assertTrue(mcDonaldsNuggetValidation.isNuggetNumberValid(0));
+    }
+
+
+    @Test
+    public void testWithFirstHundredNumbers() {
+        for (int i = 0; i <= 100; i++) {
             System.out.println("i : " + i + " - " + mcDonaldsNuggetValidation.isNuggetNumberValid(i));
         }
     }
